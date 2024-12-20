dee949e8fca068035e68d59592ba3b178e3ea392
Ashik-PUST-ICE <ashik.200607@s.pust.ac.bd>
2024-12-21 00:08:35 +0600

    work on the Admin Auth section

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AuthController;
+use App\Models\Role;
 
 Route::get('/', function () {
     return view('admin/index');
 });
+
+
+Route::get('/createAdmin',[AuthController::class,'createCustomer']);
+
+
